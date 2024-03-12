bc0f9bf4a7c2aacba66f2cef425ff155f5885192
Marc Josien <marc.josien@laposte.net>
2024-03-12 23:54:58 +0100

    MaJ : activation of openmp depends on the compile options

--- Installation/Procedure_install_Merope.sh
+++ Installation/Procedure_install_Merope.sh
@@ -15,6 +15,8 @@ mkdir BUILD-DIR INSTALL-DIR
 cd INSTALL-DIR
 mon_adresse=$PWD
 cd ../BUILD-DIR/
+## case want OpenMP for FFT
+## cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$mon_adresse -DMEROPE_USE_OPENMP_FOR_FFT=True
 cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$mon_adresse
 ### CASE DEBUG : 
 ###cmake .. -DPython_ADDITIONAL_VERSIONS=3.8 -DCMAKE_BUILD_TYPE=Debug
